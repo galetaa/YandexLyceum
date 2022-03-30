447c14f652b5debc698e1a9f192810639f94017f
Misha <mihailiy05@icloud.com>
2022-03-30 11:45:24 +0300

    Личная карточка

--- ya.py
+++ ya.py
@@ -1,78 +1,17 @@
-import sys
-from io import BytesIO
-import requests
-from PIL import Image, ImageDraw, ImageFont
+from flask import Flask, render_template
+import json
+import random
 
-toponym_to_find = " ".join(sys.argv[1:])
+app = Flask(__name__)
 
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-search_api_server = "https://search-maps.yandex.ru/v1/"
-map_api_server = "http://static-maps.yandex.ru/1.x/"
 
-geocoder_params = {
-    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-    "geocode": toponym_to_find,
-    "format": "json"}
+@app.route('/member')
+def member():
+    with open("data.json", "rt", encoding="utf8") as f:
+        data = json.loads(f.read())
+    astro = random.choice(data['crew'])
+    return render_template('member.html', data=astro)
 
-response = requests.get(geocoder_api_server, params=geocoder_params)
-if not response:
-    print('error')
-json_response = response.json()
-toponym = json_response["response"]["GeoObjectCollection"][
-    "featureMember"][0]["GeoObject"]
 
-toponym_coodrinates = toponym["Point"]["pos"]
-toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-lower_corner = list(map(float, toponym["boundedBy"]["Envelope"][
-    "lowerCorner"].split()))
-upper_corner = list(map(float, toponym["boundedBy"]["Envelope"][
-    "upperCorner"].split()))
-delta1, delta2 = '0.5', '0.5'
-
-search_params = {
-    "apikey": "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3",
-    "text": "аптека",
-    "lang": "ru_RU",
-    "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join([delta1, delta2]),
-    "type": "biz"
-}
-response = requests.get(search_api_server, params=search_params)
-if not response:
-    pass
-json_response = response.json()
-organization = json_response["features"][0]
-org_name = organization["properties"]["CompanyMetaData"]["name"]
-org_address = organization["properties"]["CompanyMetaData"]["address"]
-point = organization["geometry"]["coordinates"]
-org_point = str(point[0]) + ',' + str(point[1]) + ',' + 'flag'
-
-map_params = {
-    "l": "map",
-    "pt": ",".join(
-        [toponym_longitude, toponym_lattitude]) + '~' + org_point
-}
-response = requests.get(map_api_server, params=map_params)
-
-im = Image.open(BytesIO(response.content))
-
-length = str(int(((float(toponym_longitude) - point[0]) ** 2 + (
-        float(toponym_lattitude) - point[1]) ** 2) ** 0.5 * 111000))
-draw_text = ImageDraw.Draw(im)
-font = ImageFont.truetype('ARIALUNI.TTF', size=10)
-draw_text.text(
-    (0, 10),
-    org_name,
-    font=font,
-    fill=('#1C0606'))
-draw_text.text(
-    (0, 20),
-    org_address,
-    font=font,
-    fill=('#1C0606'))
-draw_text.text(
-    (0, 30),
-    'Расстояние: ' + length + ' метров',
-    font=font,
-    fill=('#1C0606'))
-im.show()
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
